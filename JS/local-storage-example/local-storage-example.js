98031add6211fdba4064c5d8f011d4a01b40b287
s-z-rcpto <s.z.rcpto@gmail.com>
2024-12-02 13:50:26 +0200

    Локальне сховище

--- JS/local-storage-example/local-storage-example.js
+++ JS/local-storage-example/local-storage-example.js
@@ -1,3 +1,25 @@
+document.querySelector("button").addEventListener("click", function () {
+  alert("+");
+});
+
+localStorage.setItem("number", 2);
+localStorage.setItem("bool", true);
+localStorage.setItem("string", "December");
+localStorage.setItem("array", ["q", 3, false]);
+localStorage.setItem("object", JSON.stringify({ name: "Serhii", id: 1 }));
+
+const num = localStorage.getItem("number");
+const bool = localStorage.getItem("bool");
+const str = localStorage.getItem("string");
+const arr = localStorage.getItem("array");
+const obj = JSON.parse(localStorage.getItem("object"));
+
+console.log(num, typeof num);
+console.log(bool, typeof bool);
+console.log(str, typeof str);
+console.log(arr, typeof arr);
+console.log(obj, typeof obj);
+
 // Отримуємо посилання на потрібні елементи
 const inputField = document.getElementById("inputField");
 const addButton = document.getElementById("addButton");

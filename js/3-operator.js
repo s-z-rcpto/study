37ae35ffe31837b7ee75616e049c04ea4e402b78
Serhii.A <s.z.rcpto@gmail.com>
2025-07-28 13:45:56 +0300

    Оператори

---
+++ js/3-operator.js
@@ -0,0 +1,30 @@
+const a = 10;
+const b = 5;
+
+// Арифметичні оператори
+console.log(`${a} + ${b} = ${a + b}`);
+console.log(`${a} - ${b} = ${a - b}`);
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
+
+// Остача від ділення
+console.log(`${a} % ${b} = ${a % b}`);
+console.log(`${b} % ${a} = ${b % a}`);
+console.log(`${a} % 3 = ${a % 3}`);
+console.log(`5 % 2 = ${5 % 2}`); // непарне число
+console.log(`4 % 2 = ${4 % 2}`); // парне число
+
+// Піднесення до степеня
+console.log(`${a} ** ${b} = ${a ** b}`);
+console.log(`2 у степені 3 = ${2 ** 3}`);
+console.log(`2 у степені 10 = ${2 ** 10}`);
+
+const x = "42";
+const y = "18";
+
+console.log(`Тип константи x = ${x}: ${typeof x}`);
+console.log(`Тип константи y = ${y}: ${typeof y}`);
+
+console.log(x + y);
+console.log(a + y);
+console.log(x + a);
